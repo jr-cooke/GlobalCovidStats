abcb60bb76ed8d9c26a56fb1639805442eb5c363
Justin Cooke <jrcooke2@gmail.com>
2020-03-22 18:32:28 -0300

    basic pan and zoom

--- components/WorldMap.js
+++ components/WorldMap.js
@@ -1,14 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { Choropleth } from "@nivo/geo";
 import { worldmap } from '../constants/worldmap';
 import useDataFetch from "../utils/useDataFetch";
 import useWindowSize from '../utils/useWindowSize';
 
 export default function WorldMap(){
-  const [scale, setScale] = useState(100)
+  const [scale, setScale] = useState(100);
+  const [panning, setPanning] = useState(false);
+  const [translation, setTranslation] = useState([0.5, 0.5])
   const { data, loading, error } = useDataFetch("https://corona.lmao.ninja/countries");
   const { width, height } = useWindowSize();
-
+  
+  const x = useRef();
+  const y = useRef()
+  
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
   
@@ -18,11 +23,45 @@ export default function WorldMap(){
     (max, p) => (p.value > max ? p.value : max),
     processedData[0].value
   );
-  console.log(scale)
+
+  const handleMouseDown = (e) => {
+    x.current = e.clientX;
+    y.current = e.clientY;
+    setPanning(true);
+  }
+
+  const handleMouseMove = (e) => {
+    if (panning) {
+      if (
+        translation[0] + ((e.clientX - x.current) / 1000 >= -1) &&
+        translation[0] + ((e.clientX - x.current) / 1000 <= 1) &&
+        translation[1] + ((e.clientY - y.current) / 1000 >= 1) &&
+        translation[1] + ((e.clientY - y.current) / 1000 <= 1)
+      ) {
+        setTranslation([
+          translation[0] + (e.clientX - x.current) / 1000,
+          translation[1] + (e.clientY - y.current) / 1000
+        ]);
+      }
+      x.current = e.clientX;
+      y.current = e.clientY;
+    }
+  }
+
+  const handleWheel = (e) => {
+     if (scale - e.deltaY > 50 && scale - e.deltaY < 500) {
+       setScale(scale - e.deltaY);
+     }
+  }
+
   return (
-    <div>
+    <div
+      onWheel={(e) => handleWheel(e)}
+      onMouseDown={(e) => handleMouseDown(e)}
+      onMouseUp={(e) => setPanning(false)}
+      onMouseMove={(e) => handleMouseMove(e)}
+    >
       <Choropleth
-        onWheel={(e) => { setScale(scale - e.deltaY)}}
         data={processedData}
         features={worldmap}
         domain={[0, max]}
@@ -31,7 +70,7 @@ export default function WorldMap(){
         height={height - 100}
         width={width - 100}
         projectionScale={scale}
-        projectionTranslation={[0.5, 0.5]}
+        projectionTranslation={translation}
         projectionRotation={[0, 0, 0]}
         borderWidth={0.5}
         borderColor="#152538"
